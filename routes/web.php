3c6a83e198bf68c380f64d7c255e5e4bfe3c68ed
hieudeptrai1962000 <mamthoi31@gmail.com>
2022-04-27 16:03:56 +0700

    create

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,36 +22,39 @@ Route::get('/', function () {
 //    'ShowInfo'
 //]);
 
+Route::resource('/student', 'App\Http\Controllers\HomeController');
 
-Route::get('/test',[\App\Http\Controllers\HomeController::class,'ShowInfo'] );
+
+Route::get('/test', [HomeController::class, 'ShowInfo']);
 //Route::post('/per',[\App\Http\Controllers\HomeController::class,'abc'] );
 //Route::post('/num',[\App\Http\Controllers\HomeController::class,'cba'] );
 
+//Route::post('/num',[\App\Http\Controllers\HomeController::class,'cba'] );
 
 //Route::get('/hieu', function () {
 //    return view('hieu');
 //});
-Route::get('/xinh', function () {
-    return view('form');
-});
-Route::post('/xinh', function () {
-    return view('hieu');
-});
-Route::view('hieu','hieu');
+//Route::get('/xinh', function () {
+//    return view('form');
+//});
+//Route::post('/xinh', function () {
+//    return view('hieu');
+//});
+//Route::view('hieu','hieu');
+//
+//Route::get('/hieu', function () {
+//    return view('hieu');
+//});
+//Route::get('/admin', function () {
+//    return 'admin';
+//});
 
-Route::get('/hieu', function () {
-    return view('hieu');
-});
-Route::get('/admin', function () {
-    return 'admin';
-});
-
-Route::prefix('admin')->group(function ()
-{
-    Route::get('/phuong/{id}', function ($id) {
-        $ct = "id la ";
-        $ct.= $id;
-        return $ct;
-    });
-}
-);
+//Route::prefix('admin')->group(function ()
+//{
+//    Route::get('/phuong/{id}', function ($id) {
+//        $ct = "id la ";
+//        $ct.= $id;
+//        return $ct;
+//    });
+//}
+//);
